7fc41f022c9abd833596766eb0cb35520e34f17f
Alisson Rego <alissondroid07@gmail.com>
2023-09-21 09:34:45 -0300

    Exercício CarrinhoDeCompras

---
+++ src/list/OperacoesBasicas/CarrinhoDeCompras/CarrinhoDeCompras.java
@@ -0,0 +1,52 @@
+package list.OperacoesBasicas.CarrinhoDeCompras;
+
+import list.OperacoesBasicas.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> itemsList;
+
+    public CarrinhoDeCompras() {
+        this.itemsList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        itemsList.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itemsARemover = new ArrayList<>();
+        for(Item i: itemsList) {
+            if(i.getNome().equalsIgnoreCase(nome)) {
+                itemsARemover.add(i);
+            }
+        }
+        itemsList.removeAll(itemsARemover);
+    }
+
+    public double calcularValorTotal() {
+        double valorTotal = 0d;
+        for(Item i: itemsList) {
+            double valorItem = i.getPreco() * i.getQuantidade();
+            valorTotal += valorItem;
+        }
+        return valorTotal;
+    }
+
+    public void exibirItens() {
+        System.out.println(itemsList);
+    }
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras cart = new CarrinhoDeCompras();
+
+        cart.adicionarItem("Boneca Barbie", 12.90, 1);
+        cart.adicionarItem("Carrinho Hotwheels", 4.90, 1);
+        cart.adicionarItem("Bola Futebol", 10.30, 1);
+
+        cart.removerItem("Boneca Barbie");
+        cart.exibirItens();
+    }
+}
---
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,39 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    // Adiciona uma nova tarefa
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    // Remove uma tarefa
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t: tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+        }
+
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    // Retorna o número de tarefas
+    public int obterNumeroDeTarefas() {
+        return tarefaList.size();
+    }
+
+    // Imprime a descrição da tarefa
+    public void obterDescricaoDeTarefas() {
+        System.out.println(tarefaList);
+    }
+}
